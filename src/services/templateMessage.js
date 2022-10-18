c871f100ad47d832fb192e105a06b3f01568244a
Gener Castillon <genercastillon@gmail.com>
2022-10-18 11:48:32 +0800

    update

--- src/services/templateMessage.js
+++ src/services/templateMessage.js
@@ -56,7 +56,7 @@ let classMode = [
 
 let trainingDetails = (i = 0) => {
     return {
-        "text": programs[i].toUpperCase() + " (" + trainingHours[i] + ")\n\n•" + classMode[i] + "\n\nTo enroll present the following documents:\n1. Copy of ID\n2. Birth Certificate\n3. Duly Accomplished Application Form\n\nRegarding for the tuition fee, schedule and other details please direct us through by calling 8569324 local 146 or personal here in our office. Thank you" 
+        "text": programs[i].toUpperCase() + " (" + trainingHours[i] + ")\n\n" + classMode[i] + ", 8 hrs per day.\n\nTo enroll present the following documents:\n1. Copy of ID\n2. Birth Certificate\n3. Duly Accomplished Application Form\n\nRegarding for the tuition fee, schedule and other details please direct us through by calling 8569324 local 146 or personal here in our office. Thank you" 
     };
 };
 
